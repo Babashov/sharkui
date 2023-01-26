f8fc272c88826ba17482dea4001ee5d21ed86e54
Babashov Mahammad <babashov.info@gmail.com>
2023-01-26 14:46:55 +0400

    initial

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
 import { ExampleComponent } from 'sharkui'
+import { Button } from 'sharkui'
 import 'sharkui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <ExampleComponent text="Create React Library Example 😄" />
+      <Button text='Click Button text' selfClickEvent={(e)=>{console.log(e.target)}} />
+    </>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import styles from './styles.module.css'
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
+
+export const Button = ({text,selfClickEvent})=>{
+  return <button onClick={selfClickEvent}>{text}</button>
+}
